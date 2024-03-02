79effc5bc9dcd51ec2ccbea4330ab2790ed5e294
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-03-02 14:52:56 +0530

    hosting changes

--- main.js
+++ main.js
@@ -10,37 +10,26 @@ import cartRoutings from "./customer/cart/index.js"
 import sizesRoutings from "./admin/Frames size/index.js"
 import thicknessRoutings from "./admin/Frames thickness/index.js"
 import priceRoutings from "./admin/Frames Price/index.js"
-
-
 const app = express()
 dotenv.config()
 app.use(express.json());
 app.use(cors())
-
 let corsOptions = {
-    origin: [ 'http://localhost:5000', ]
+    origin: ['http://78.142.47.247:8000',]
 };
-
 app.use('/admin', cors(corsOptions), adminRoutings);
 app.use('/customer', cors(corsOptions), customerRoutings);
-app.use( '/photo', cors(corsOptions), uploadphotoRoutings);
-app.use( '/cart', cors(corsOptions), cartRoutings);
-app.use( '/size', cors(corsOptions), sizesRoutings);
-app.use( '/thickness', cors(corsOptions), thicknessRoutings);
-app.use( '/price', cors(corsOptions), priceRoutings);
+app.use('/photo', cors(corsOptions), uploadphotoRoutings);
+app.use('/cart', cors(corsOptions), cartRoutings);
+app.use('/size', cors(corsOptions), sizesRoutings);
+app.use('/thickness', cors(corsOptions), thicknessRoutings);
+app.use('/price', cors(corsOptions), priceRoutings);
 app.use(verifyToken);
-
 mongoose.connect(process.env.DB_URL)
-    .then( console.log('db connected succesfully'))
-    .catch(err =>{
-            console.log('error',err)
+    .then(console.log('db connected succesfully'))
+    .catch(err => {
+        console.log('error', err)
     })
-
-//port    
-
-app.listen(5000,()=>{
-    console.log(`Server on http://localhost:${process.env.port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server on http://78.142.47.247:${process.env.PORT}`)
 });
-
-
-
